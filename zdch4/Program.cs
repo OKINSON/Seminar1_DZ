9bf602cac2c9dcfdb8df7ffa49baa4169eb8983c
KONSTANTIN <okink@yandex.ru>
2023-01-16 17:45:04 +0300

    задача4

--- zdch4/Program.cs
+++ zdch4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+    Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+    {
+        max = b;
+}
+if (c > max)
+    {
+        max = c;
+}
+Console.WriteLine("Наибольшее из введённых чисел: " + max);
