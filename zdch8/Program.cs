58ce2f19f773dc023e676484206fd28c4a77201e
KONSTANTIN <okink@yandex.ru>
2023-01-16 18:28:00 +0300

    задача8

--- zdch8/Program.cs
+++ zdch8/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int i = 1;
+bool not = true;
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+if (i % 2 != 1)
+{
+Console.Write(i + ", ");
+not = false;
+}
+i++;
+}
+if (not)
+{
+Console.WriteLine("Откуда ж тут четные?");
+}
