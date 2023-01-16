63bfe380e6b257225b53e3ce5d9ec09d4dc1ab07
KONSTANTIN <okink@yandex.ru>
2023-01-16 16:53:22 +0300

    задача2

--- zdch2/Program.cs
+++ zdch2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите превое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+Console.WriteLine(n);
+else if (n < m)
+Console.WriteLine(m);
+else
+ Console.WriteLine("=");
