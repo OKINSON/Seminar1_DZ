428eb8615d088d6506cf12b5dbb6b1dffc5894fa
KONSTANTIN <okink@yandex.ru>
2023-01-20 00:26:19 +0300

    Задача15

---
+++ zdch15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели ");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day >= 1 && Day <=7) {
+	if (Day >= 6) {
+		Console.Write("выходной");
+	} else {
+		Console.Write("рабочий");
+	}
+} else {
+	Console.Write("Hееее, это за пределами нашей недели");
+}
