e82834b64526cc6685b1afd1f87ed0561ef7614c
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-01-26 15:08:35 +0300

    25

---
+++ zdch25/Program.cs
@@ -0,0 +1,14 @@
+﻿//  задача 25
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+    for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("Результат : " + step);
