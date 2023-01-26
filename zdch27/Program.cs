e8948b200b01855c3de980e014c4729a974fd438
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-01-26 15:09:44 +0300

    27

---
+++ zdch27/Program.cs
@@ -0,0 +1,13 @@
+﻿//  задача 27
+
+Console.Clear();
+Console.WriteLine("введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+    while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("сумма цифр в этом числе: " + sum);
