19456602bcb4be995843caa78bffac6dc89956c2
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-01-26 15:11:57 +0300

    29

---
+++ zdch29/Program.cs
@@ -0,0 +1,7 @@
+﻿//  задача 29
+
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(00, 11); // [-10, 10]
+Console.WriteLine($"Результат: [{string.Join(" ", array)}]");
