17fc497fd91ef4f4e8266a3205cbee2cf2871631
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-01-27 11:29:54 +0300

    fix27

--- zdch27/Program.cs
+++ zdch27/Program.cs
@@ -1,13 +1,22 @@
 ﻿//  задача 27
 
 Console.Clear();
-Console.WriteLine("введите число: ");
-int i = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-    while (i > 0)
+// Console.WriteLine("введите число: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+//     while (i > 0)
+// {
+// int num = i % 10;
+// i = i / 10;
+// sum = sum + num;
+// }
+// Console.WriteLine("сумма цифр в этом числе: " + sum);
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 0;
+while (n > 0)
 {
-int num = i % 10;
-i = i / 10;
-sum = sum + num;
+    result = result + n % 10; // result += n %  10;
+    n = n / 10; // n /= 10;
 }
-Console.WriteLine("сумма цифр в этом числе: " + sum);
+Console.WriteLine(result);
