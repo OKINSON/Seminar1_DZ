ca648cc27b3dc2db7e1fba64b624e15a5222aa8d
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-01-27 11:02:17 +0300

    fix zdch25

--- zdch25/Program.cs
+++ zdch25/Program.cs
@@ -1,14 +1,10 @@
 ﻿//  задача 25
 
 Console.Clear();
-Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите степень: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int step = a;
-
-    for (int i = 1; i < b; i++)
-{
-step = step * a;
-}
-Console.WriteLine("Результат : " + step);
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= m; i++)
+    result = result * n; // result *= n;
+Console.WriteLine(result);
