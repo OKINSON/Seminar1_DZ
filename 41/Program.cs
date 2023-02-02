2c921ce84b6b173e5ae884a30880683ecf0dd6cf
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-02-02 17:03:51 +0300

    41

---
+++ 41/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите элементы (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Колличество элементов больше 0: {count}");
