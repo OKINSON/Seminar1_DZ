7f867ab3792d6ef2eabc55720583bf02657b2a21
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-02-03 11:56:20 +0300

    41 fix

--- 41/Program.cs
+++ 41/Program.cs
@@ -1,5 +1,20 @@
-﻿Console.Clear();
-Console.Write("Введите элементы (через пробел): ");
+﻿
+    Console.Clear();
+Console.Write("Введите числа (через пробел): ");
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = arr.Count(x => x > 0);
+
 Console.WriteLine($"Колличество элементов больше 0: {count}");
+
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+// for (int i = 0; i < n; i++)
+// {
+//     Console.Write("Введите число: ");
+//     int x = Convert.ToInt32(Console.ReadLine());
+//     if (x > 0)
+//         count++; // count = count + 1;
+// }
+// Console.WriteLine($"Результат: {count}");
