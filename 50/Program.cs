a136bc751000dca9025b22b1aa51096ecd3f8e21
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-02-07 10:17:05 +0300

    50

--- 50/Program.cs
+++ 50/Program.cs
@@ -1,27 +1,60 @@
-﻿Console.Clear();
-Console.Write("Введите строку: ");
-int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;  // чтоб позицию, а не индекс
-Console.Write("Введите столбец: ");
-int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-int n = 4; 
-int m = 3; 
-Random random = new Random();
-int[,] arr = new int[n, m];
-Console.WriteLine("Исходный массив: ");
-for (int i = 0; i < arr.GetLength(0); i++)
+﻿// Console.Clear();
+// Console.Write("Введите строку: ");
+// int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;  // чтоб позицию, а не индекс
+// Console.Write("Введите столбец: ");
+// int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+// int n = 4; 
+// int m = 3; 
+// Random random = new Random();
+// int[,] arr = new int[n, m];
+// Console.WriteLine("Исходный массив: ");
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     arr[i, j] = random.Next(10, 99);
+// Console.Write("{0} ", arr[i, j]);
+// }
+// Console.WriteLine();
+// }
+//     if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+// {
+// Console.WriteLine("Элемент не существует  ");
+// }
+//     else
+// {
+//     Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+// }
+
+Console.Clear();
+void InputMatrix(double[,] matrix)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+double SearchElementToMatrix(double[,] matrix, int row, int column)
 {
-    arr[i, j] = random.Next(10, 99);
-Console.Write("{0} ", arr[i, j]);
+    if (matrix.GetLength(0) > row && matrix.GetLength(1) > column && row >= 0 && column >= 0)
+        return matrix[row, column];
+    return 0;
 }
-Console.WriteLine();
-}
-    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
-{
-Console.WriteLine("Элемент не существует  ");
-}
-    else
-{
-    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
-}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+int[] coord = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+Console.Write("Введите позицию элемента: ");
+double result = SearchElementToMatrix(matrix, coord[0] - 1, coord[1] - 1);
+if (result != 0)
+    Console.WriteLine(result);
+else
+    Console.WriteLine("Такого числа нет");
