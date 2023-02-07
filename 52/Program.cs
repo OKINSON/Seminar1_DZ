9c458dd94956f0921eb9eeda26447329dd60448b
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-02-07 10:23:38 +0300

    52

--- 52/Program.cs
+++ 52/Program.cs
@@ -1,23 +1,59 @@
-﻿Console.Clear();
-Random random = new Random();
-int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
-for (int i = 0; i < arr.GetLength(0); i++)
+﻿// Console.Clear();
+// Random random = new Random();
+// int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = random.Next(1, 10);
+//         Console.Write(arr[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("___________________");
+// Console.WriteLine(arr.GetLength( 0 ));
+// for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         sum += arr[i, j];
+//     }
+//     Console.Write($"{ sum / arr.GetLength(0)} ");
+// }
+
+Console.Clear();
+void InputMatrix(double[,] matrix)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        arr[i, j] = random.Next(1, 10);
-        Console.Write(arr[i, j] + " ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
-Console.WriteLine("___________________");
-Console.WriteLine(arr.GetLength( 0 ));
-for (int j = 0; j < arr.GetLength(1); j++)
+
+
+void ReleaseMatrix(double[,] matrix)
 {
-    double sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        sum += arr[i, j];
+        double summa = 0;
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            summa += matrix[i, j];
+        }
+        Console.WriteLine($"Результат ср. ариф. {j + 1} = {summa / matrix.GetLength(0)}");
     }
-    Console.Write($"{ sum / arr.GetLength(0),2} ");
 }
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
