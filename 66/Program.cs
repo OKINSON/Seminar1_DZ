f280f76f6623d5ee45455793975ec401cd720a62
OKINSON <121186122+OKINSON@users.noreply.github.com>
2023-02-11 14:44:16 +0300

    66

---
+++ 66/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void NtoMSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    NtoMSum(numberM, numberN, sum);
+}
+
+NtoMSum(numberM, numberN, 0);
